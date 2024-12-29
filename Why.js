1e6c22d9294100b16b9a0e2ce9d502dcc0cf2468
Cole Breen <144734306+ColeBreen99@users.noreply.github.com>
2024-12-29 13:29:26 -0500

    commit 5
    All pages built for mobile use!

--- Why.js
+++ Why.js
@@ -8,11 +8,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-// Get the hamburger menu and dropdown elements
-const hamburgerMenu = document.getElementById('hamburger-menu');
-const dropdownMenu = document.querySelector('.dropdown-menu');
+        // Get the hamburger menu and dropdown elements
+        const hamburgerMenu = document.getElementById('hamburger-menu');
+        const dropdownMenu = document.querySelector('.dropdown-menu');
+    
+            // Add event listener to toggle the dropdown on hamburger click
+        hamburgerMenu.addEventListener('click', function() {
+        dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+        });
 
-// Add event listener to toggle the dropdown on hamburger click
-hamburgerMenu.addEventListener('click', function() {
-    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
-});
+
